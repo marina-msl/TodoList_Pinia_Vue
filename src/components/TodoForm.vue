fbb5732f79d358fb06a3bee8dca82aacd459ef4f
Marina Leide <marina.msleide@gmail.com>
2025-06-30 22:53:55 -0300

    Implementing Todo List and working with Pinia

---
+++ src/components/TodoForm.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import { useTodoListStore } from '@/stores/TodoList'
+
+const todo = ref('')
+
+const store = useTodoListStore()
+
+function addItemAndClear(item) {
+    if (item.lenght == 0) {
+        return
+    }
+    store.addTodo(item)
+    todo.value = ''
+}
+
+</script>
+
+<template>
+    <div>
+        <form @submit.prevent="addItemAndClear(todo)">
+            <input v-model="todo" type="text" /><button>Add</button>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { useTodoListStore } from '../stores/todoList'
+import { storeToRefs } from 'pinia'
+
+const store = useTodoListStore()
+
+const { todoList } = storeToRefs(store)
+</script>
+
+<template>
+    <div v-for="todo in todoList" :key="todo.id" class="item">  
+        <div class="content">
+            <span>{{ todo.item }}</span>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+</style>
